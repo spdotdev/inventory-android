e9f97043b219cb8b3555e0482a08ebb0da01c711
S Plotnikov <stplotnikov@gmail.com>
2026-06-29 21:32:33 +0200

    chore: bump version to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "dev.scuttle.inventory"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Base URL of the inventory API. Override per build type / flavor as needed;
